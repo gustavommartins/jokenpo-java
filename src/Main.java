0e8efa6211ca99acec42671649f3e649546a5019
Gustavo Martins <gustavommartinsctg@outlook.com>
2025-08-20 12:41:04 -0300

    feat: enhance game loop to allow replaying in Rock, Paper, Scissors

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,34 @@ public class Main {
 
         Jogo jogo = new Jogo(jogador, computador);
 
-        jogo.iniciar();
-
+        boolean iscontinuarJogo = true;
+        while (iscontinuarJogo) {
+            jogo.iniciar(scanner);
+            System.out.println("\nDeseja jogar novamente? (s/n)");
+            String resposta = scanner.nextLine().trim().toLowerCase();
+            iscontinuarJogo = isProximaPartida(resposta, scanner);
+        }
     }
+
+    private static boolean isProximaPartida(String resposta, Scanner scanner) {
+        boolean isProxima;
+        switch (resposta) {
+            case "s" -> {
+                System.out.println("Iniciando uma nova partida...");
+                isProxima = true;
+            }
+            case "n" -> {
+                System.out.println("Obrigado por jogar! Até a próxima!");
+                isProxima = false;
+            }
+            default -> {
+                System.out.println("Resposta inválida! Por favor, digite 's' para sim ou 'n' para não.");
+                String novaResposta = scanner.nextLine().trim().toLowerCase();
+                return isProximaPartida(novaResposta, scanner);
+            }
+        }
+        return isProxima;
+    }
+
+
 }
